7fc1c41de5c5e1be2d83af62cd847a5c02366658
Silin Na <n1207n@gmail.com>
2015-01-10 01:12:47 -0800

    Added a simple listview for ContactListActivity. + Added Contact class.

---
+++ app/src/main/java/silin/tutorial/mycontacts/Contact.java
@@ -0,0 +1,13 @@
+package silin.tutorial.mycontacts;
+
+public class Contact {
+    private String mName;
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+}
--- app/src/main/java/silin/tutorial/mycontacts/ContactListActivity.java
+++ app/src/main/java/silin/tutorial/mycontacts/ContactListActivity.java
@@ -1,9 +1,17 @@
 package silin.tutorial.mycontacts;
 
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 
 public class ContactListActivity extends ActionBarActivity {
@@ -12,6 +20,31 @@ public class ContactListActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_list);
+
+        ArrayList<Contact> contacts = new ArrayList<>();
+        Contact contact1 = new Contact();
+        contact1.setName("Silin Na");
+        contacts.add(contact1);
+
+        ListView listView = (ListView) findViewById(R.id.contact_list_view);
+        listView.setAdapter(new ContactAdapter(contacts));
+    }
+
+    private class ContactAdapter extends ArrayAdapter<Contact> {
+        private ContactAdapter(ArrayList<Contact> contacts) {
+            super(ContactListActivity.this, R.layout.contact_list_row, R.id.contact_row_name, contacts);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            convertView = super.getView(position, convertView, parent);
+
+            Contact contact = getItem(position);
+            TextView nameTextView = (TextView) convertView.findViewById(R.id.contact_row_name);
+            nameTextView.setText(contact.getName());
+            
+            return convertView;
+        }
     }
 
 
