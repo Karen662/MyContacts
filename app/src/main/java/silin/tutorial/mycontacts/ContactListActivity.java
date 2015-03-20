1c6cd4007c563a3ac5e6491d2bd683a0a93430e6
Silin Na <n1207n@gmail.com>
2015-03-19 21:24:11 -0700

    Created activity_contact_list for tablet layout. + Added a logic to attach mContactViewFragment to the frame layout if the tablet layout is there.

--- app/src/main/java/silin/tutorial/mycontacts/ContactListActivity.java
+++ app/src/main/java/silin/tutorial/mycontacts/ContactListActivity.java
@@ -7,15 +7,34 @@ import android.support.v7.app.ActionBarActivity;
 
 public class ContactListActivity extends ActionBarActivity implements ContactListFragment.Contract {
 
+    private ContactListFragment mContactListFragment;
+
+    private ContactViewFragment mContactViewFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_list);
 
-        if (getFragmentManager().findFragmentById(R.id.list_fragment_container) == null)
+        mContactListFragment = (ContactListFragment) (getFragmentManager().findFragmentById(R.id.list_fragment_container));
+
+        if (mContactListFragment == null)
         {
+            mContactListFragment = new ContactListFragment();
+
             getFragmentManager().beginTransaction()
-                    .add(R.id.list_fragment_container, new ContactListFragment())
+                    .add(R.id.list_fragment_container, mContactListFragment)
+                    .commit();
+        }
+
+        mContactViewFragment = (ContactViewFragment) (getFragmentManager().findFragmentById(R.id.view_fragment_container));
+
+        if (mContactViewFragment == null && findViewById(R.id.view_fragment_container) != null)
+        {
+            mContactViewFragment = new ContactViewFragment();
+
+            getFragmentManager().beginTransaction()
+                    .add(R.id.view_fragment_container, mContactViewFragment)
                     .commit();
         }
     }
